5736fd228dc589b3986c476726809727a53960d6
Simon Crase <simon@greenweaves.nz>
2022-12-12 21:28:32 +1300

    #Closed #8

--- src/visualize.py
+++ src/visualize.py
@@ -28,7 +28,26 @@
 from argparse          import ArgumentParser
 from dicomsdl          import open
 from matplotlib.pyplot import figure, show
-from numpy             import all, arange, histogram, log,  where, zeros
+from numpy             import all, arange, flip, histogram, log,  where, zeros
+
+def get_image(file,path='data'):
+    '''
+    Read image from specified file, verify keywords are consistent with image, and normalize so background is high
+    '''
+    dataset                   = open(f'../{path}/{file}.dcm')
+    pixels                    = dataset.pixelData()
+    PhotometricInterpretation = dataset.getDataElement('PhotometricInterpretation').value()
+    SamplesPerPixel           = dataset.getDataElement('SamplesPerPixel').value()
+    ImageLaterality           = dataset.getDataElement('ImageLaterality').value()
+    Rows                      = dataset.getDataElement('Rows').value()
+    Columns                   = dataset.getDataElement('Columns').value()
+    m,n                       = pixels.shape
+    assert m==dataset.getDataElement('Rows').value() and n==dataset.getDataElement('Columns').value()
+    if dataset.getDataElement('PhotometricInterpretation').value()=='MONOCHROME2':
+        pixels = pixels.max() - pixels
+    if dataset.getDataElement('ImageLaterality').value()=='R':
+        pixels = flip(pixels,axis=1)
+    return pixels
 
 def get_bounds(pixel_array):
     '''
@@ -57,7 +76,6 @@ def get_bounds(pixel_array):
     xmin,ymin = 0,0
     xmax,ymax = pixel_array.shape
 
-
     while is_background(pixel_array[xmin,:]):
         xmin+= 1
     while is_background(pixel_array[:,ymin]):
@@ -67,7 +85,7 @@ def get_bounds(pixel_array):
     while is_background(pixel_array[:,ymax-1]):
         ymax-= 1
 
-    return xmin,ymin,xmax,ymax, background, background_low
+    return xmin,ymin,xmax,ymax, background
 
 def get_centre_of_mass(pixels,step=16):
     '''
@@ -105,7 +123,7 @@ def get_path(p0,p1,scaled):
         n1 = int(x0)
         must_reverse = True
 
-    # FEXME: this code has too many special cases for my liking
+    # FIXME: this code has too many special cases for my liking
     if abs(m1-m0)>0:
         xs = [i for i in range(m0,m1)]
         ys = [min(n0 + int((i-m0)*(n1-n0)/(m1-m0)),n-1) for i in xs]
@@ -134,39 +152,37 @@ if __name__=='__main__':
     args  = parser.parse_args()
 
     for file in args.files:
-        dataset                                       = open(f'../data/{file}.dcm')
-        pixels                                        = dataset.pixelData()
-        xmin,ymin,xmax,ymax,background,background_low = get_bounds(pixels)
-
-        m1       = pixels[xmin:xmax,ymin:ymax].min()
-        m2       = pixels[xmin:xmax,ymin:ymax].max()
-        scaled   = (pixels[xmin:xmax,ymin:ymax]-m1)/(m2-m1)
-        scaled_b = (background - m1)/(m2-m1)
-        x_c, y_c = get_centre_of_mass(scaled)
-        m,n      = scaled.shape
-        ends     = [[0,0], [int(x_c),0],
-                    [m,0], [m,int(y_c)],
-                    [m,n], [int(x_c),n],
-                    [0,n], [0,int(y_c)]]
+        pixels                         = get_image(file)
+        xmin,ymin,xmax,ymax,background = get_bounds(pixels)
+        m1                             = pixels[xmin:xmax,ymin:ymax].min()
+        m2                             = pixels[xmin:xmax,ymin:ymax].max()
+        scaled_pixels                  = (pixels[xmin:xmax,ymin:ymax]-m1)/(m2-m1)
+        scaled_background              = (background - m1)/(m2-m1)
+        x_c, y_c                       = get_centre_of_mass(scaled_pixels)
+        m,n                            = scaled_pixels.shape
+        ends                           = [[0,0], [int(x_c),0],
+                                          [m,0], [m,int(y_c)],
+                                          [m,n], [int(x_c),n],
+                                          [0,n], [0,int(y_c)]]
 
         fig  = figure(figsize=(12,8))
         ax1  = fig.add_subplot(3,4,1)
-        ax1.imshow(pixels, cmap = 'gray')
-        ax2  = fig.add_subplot(3,4,2)
-        ax2.imshow(pixels[xmin:xmax,ymin:ymax], cmap = 'gray')
-        ax3  = fig.add_subplot(3,4,3)
-        ax3.imshow(scaled, cmap = 'gray')
+        ax1.imshow(scaled_pixels, cmap = 'gray')
 
         for k,(x,y) in enumerate(ends):
-            ax3.plot([y_c,y],[x_c,x],
+            ax1.plot([y_c,y],[x_c,x],
                      c         = XKCD_COLOURS[k],
                      marker    = '.',
                      linewidth = 1)
 
-            xs,ys,zs = get_path((x_c,y_c),(x,y),scaled)
-            ax4  = fig.add_subplot(3,4,k+4)
-            ax4.plot(zs,c=XKCD_COLOURS[k])
-            ax4.hlines(scaled_b,0,len(zs), colors='xkcd:black',linestyles='dotted', color='xkcd:black')
+            xs,ys,zs = get_path((x_c,y_c),(x,y),scaled_pixels)
+            ax4  = fig.add_subplot(3,4,k+2)
+            ax4.plot(zs, c = XKCD_COLOURS[k])
+            ax4.hlines(scaled_background,0,len(zs),
+                       colors     = 'xkcd:black',
+                       linestyles = 'dotted',
+                       color      = 'xkcd:black')
+        show()
 
     if args.show:
         show()
