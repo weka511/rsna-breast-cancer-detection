4ae1403c8e5ad2df09532f1d94ef77223552b4a1
Simon Crase <simon@greenweaves.nz>
2022-12-14 19:38:04 +1300

    #9 CranioCaudal segmentation OK except 288394860

--- src/segment.py
+++ src/segment.py
@@ -21,16 +21,22 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from abc               import ABC,abstractmethod
 from argparse          import ArgumentParser
 from loader            import Loader
-from matplotlib.pyplot import figure, show
-from numpy             import flip
+from matplotlib.pyplot import close, figure, show
+from numpy             import all, any, flip
 
-class Segmenter:
+class Segmenter(ABC):
     @classmethod
     def Create(cls,view):
         if view=='CC':
-            return Segmenter()
+            return CranioCaudalSegmenter()
+    @abstractmethod
+    def segment(self,pixels,laterality='L'):
+        ...
+
+class CranioCaudalSegmenter(Segmenter):
     def __init__(self):
         pass
     def segment(self,pixels,laterality='L'):
@@ -39,11 +45,38 @@ class Segmenter:
         if laterality=='R':
             pixels = flip(pixels,axis=1)
         return pixels
+    def get_bounds(self,pixels,epsilon=0.01):
+        threshold = pixels.max() - epsilon
+        m,n       = pixels.shape
+        m0,n0     = 0,0
+        n1        = 1
+        m1        = m-1
+
+        while n1<n: #red
+            if all(pixels[: ,n1]>threshold):
+                break
+            n1 += 1
+        while m0<m: #green
+            if any(pixels[m0,n0:n1]<threshold):
+                break
+            m0 += 1
+        while m1>m0:
+            if any(pixels[m1,n0:n1]<threshold):
+                break
+            m1 -= 1
+        return m0,n0,m1,n1
+
+class  MediolateralObliqueSegmenter(Segmenter):
+    def __init__(self):
+        pass
+    def segment(self,pixels,laterality='L'):
+        pass
 
 if __name__=='__main__':
     parser = ArgumentParser(__doc__)
     parser.add_argument('image_ids', nargs='+', type=int)
     parser.add_argument('--show', default=False, action='store_true')
+    parser.add_argument('--step', default=False, action='store_true')
     args  = parser.parse_args()
 
     loader   = Loader()
@@ -51,10 +84,23 @@ if __name__=='__main__':
         pixels,laterality,view = loader.get_image(image_id=image_id)
         segmenter              = Segmenter.Create(view)
         if segmenter==None: continue
-        pixels = segmenter.standardize_orientation(pixels,laterality=laterality)
+        pixels      = segmenter.standardize_orientation(pixels,laterality=laterality)
+        m0,n0,m1,n1 = segmenter.get_bounds(pixels)
 
         fig      = figure(figsize=(12,8))
-        ax1      = fig.add_subplot(1,1,1)
+        ax1      = fig.add_subplot(1,2,1)
         fig.suptitle(f'{image_id} {laterality} {view}')
         ax1.imshow(pixels, cmap = 'gray')
-    show()
+        ax1.axvline(n1,c='xkcd:red',linestyle='dotted')
+        ax1.axhline(m0,c='xkcd:red',linestyle='dotted')
+        ax1.axhline(m1,c='xkcd:red',linestyle='dotted')
+        ax2 = fig.add_subplot(1,2,2)
+        ax2.imshow(pixels[m0:m1,n0:n1], cmap = 'gray')
+        if args.step:
+            show()
+        else:
+            if not args.show:
+                close(fig)
+
+        if args.show and not args.step:
+            show()
