91b2d579e20c184467a9a92903c26bc696f453db
Simon Crase <simon@greenweaves.nz>
2022-12-15 14:56:09 +1300

    #10 Save output in batch file

--- src/download.py
+++ src/download.py
@@ -29,7 +29,7 @@ def download(name,out):
     if parts[0].startswith(TYPE):
         out.write(f'kaggle competitions download -f {name} rsna-breast-cancer-detection\n')
 
-with open('download.bat','w') as out,open('../data/files.txt') as f:
+with open('download.bat','w') as out,open(r'D:\data\rsna-breast-cancer-detection/files.txt') as f:
     for line in f:
         name = line.split()[0]
         if name.endswith('dcm'):
