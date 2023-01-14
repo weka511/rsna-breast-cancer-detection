1892b7397df37c02df127ec37726a921e0963218
Simon Crase <simon@greenweaves.nz>
2023-01-15 09:48:27 +1300

    #15 new class for VOILUT

--- src/loader.py
+++ src/loader.py
@@ -23,17 +23,16 @@
 
 '''Read image from restructured data on drive D'''
 
+from abc               import ABC,abstractmethod
 from dicomsdl          import open
 from matplotlib.pyplot import figure, show
-from numpy             import uint8
+from numpy             import exp, uint8
 from os.path           import exists, join
 from pandas            import read_csv
 
-class Window:
+class VOILUT(ABC):
     '''
-    Apply windowing as specified in dicom file
-
-    snarfed from https://www.kaggle.com/code/omission/eda-view-dicom-images-with-correct-windowing/notebook
+    https://dicom.nema.org/medical/dicom/2018b/output/chtml/part03/sect_C.11.2.html#equation_C.11-1
     '''
     @staticmethod
     def get_first_element(x):
@@ -43,20 +42,44 @@ class Window:
         else:
             return x
 
+    @classmethod
+    def create(cls,ds):
+        VOILUTFunction   = ds['VOILUTFunction']
+        if VOILUTFunction=='SIGMOID':
+            return Sigmoid(ds)
+        else:
+            return Linear(ds)
+
     def __init__(self,ds):
         self.RescaleIntercept = ds.getDataElement('RescaleIntercept').value()
         self.RescaleSlope     = ds.getDataElement('RescaleSlope').value()
-        self.WindowCenter     = Window.get_first_element(ds.getDataElement('WindowCenter').value())
-        self.WindowWidth      = Window.get_first_element(ds.getDataElement('WindowWidth').value())
-        self.VOILUTFunction   = ds['VOILUTFunction']
+        self.WindowCenter     = VOILUT.get_first_element(ds.getDataElement('WindowCenter').value())
+        self.WindowWidth      = VOILUT.get_first_element(ds.getDataElement('WindowWidth').value())
+    def scale(self,img):
+        ...
+
+class Linear(VOILUT):
+    def __init__(self,ds):
+        super().__init__(ds)
         self.img_min          = self.WindowCenter - self.WindowWidth//2
         self.img_max          = self.WindowCenter + self.WindowWidth//2
-
+    @abstractmethod
     def scale(self,img):
         img_scaled                          = img*self.RescaleSlope +self.RescaleIntercept
         img_scaled[img_scaled<self.img_min] = self.img_min
         img_scaled[img_scaled>self.img_max] = self.img_max
         return img_scaled
+
+class Sigmoid(VOILUT):
+    def __init__(self,ds):
+        super().__init__(ds)
+        BitsStored       = ds.getDataElement('BitsStored').value()
+        self.OutputRange = 2**BitsStored-1
+
+    def scale(self,img):
+        scaled1 = (img-self.WindowCenter)/self.WindowWidth
+        return self.OutputRange/(1+exp(-4*scaled1))
+
 
 class Loader:
     '''
@@ -102,62 +125,30 @@ class Loader:
             row        = self.master[self.master['image_id']==image_id]
             patient_id = int(row['patient_id'])
 
-        ds                   = open(self.get_image_file_name(patient_id,image_id))
+        ds  = open(self.get_image_file_name(patient_id,image_id))
 
         if show_pixel_data_info:
-            for key in ds:
-                print (key,key.repr())
             dump = ds.dump()
             print (dump)
             for key,value in ds.getPixelDataInfo().items():
                 print (key,value)
+
         img                       = ds.pixelData()
 
         PhotometricInterpretation = ds.getDataElement('PhotometricInterpretation').value()
         SamplesPerPixel           = ds.getDataElement('SamplesPerPixel').value()
-        ImageLaterality           = ds.getDataElement('ImageLaterality').value()
-        bits_stored = ds["BitsStored"]
-        voi_lut_function = ds["VOILUTFunction"]
-        window = Window(ds)
+        window                    = VOILUT.create(ds)
         m,n                       = img.shape
         assert m==ds.getDataElement('Rows').value() and n==ds.getDataElement('Columns').value()
 
-        if should_apply_windowing:
-            img = window.scale(img)
-
         view        = row['view'].values[0]
-        laterality  = ds.getDataElement('ImageLaterality').value(),
-        laterality2 = row['laterality'].values[0]
-        assert laterality == (laterality2,)
+        ImageLaterality  = ds.getDataElement('ImageLaterality').value(),
+        RowLaterality = row['laterality'].values[0]
+        assert ImageLaterality == (RowLaterality,)
         cancer = row['cancer']
         img = self.force_monochrome1(PhotometricInterpretation,img)
-        return self.normalize(img) if should_apply_windowing else img,laterality2,view,int(cancer)
+        return self.normalize(window.scale(img)) if should_apply_windowing else img,RowLaterality,view,int(cancer)
 
-
-
-    def apply_windowing(self,ds,img):
-        '''
-        Apply windowing as specified in disom file
-
-        snarfed from https://www.kaggle.com/code/omission/eda-view-dicom-images-with-correct-windowing/notebook
-        '''
-        def get_first_element(x):
-            if type(x)==list:
-                print (x)
-                return x[0]
-            else:
-                return x
-
-        RescaleIntercept = ds.getDataElement('RescaleIntercept').value()
-        RescaleSlope     = ds.getDataElement('RescaleSlope').value()
-        img              = img*RescaleSlope +RescaleIntercept
-        WindowCenter     = get_first_element(ds.getDataElement('WindowCenter').value())
-        WindowWidth      = get_first_element(ds.getDataElement('WindowWidth').value())
-        img_min          = WindowCenter - WindowWidth//2
-        img_max          = WindowCenter + WindowWidth//2
-        img[img<img_min] = img_min
-        img[img>img_max] = img_max
-        return img
 
     def force_monochrome1(self,photometricInterpretation,img):
         '''
@@ -189,7 +180,9 @@ def get_all_images(path = r'D:\data\rsna-breast-cancer-detection',
 if __name__=='__main__':
     loader   = Loader()
     img,laterality,view,cancer = loader.get_image(image_id=797737008)
-    img0,_,_,_ = loader.get_image(image_id=797737008,should_apply_windowing=False,show_pixel_data_info=True)
+    img0,_,_,_ = loader.get_image(image_id               = 797737008,
+                                  should_apply_windowing = False,
+                                  show_pixel_data_info   = True)
     fig      = figure(figsize=(12,8))
     ax1      = fig.add_subplot(2,2,1)
     ax1.imshow(img0, cmap = 'gray')
